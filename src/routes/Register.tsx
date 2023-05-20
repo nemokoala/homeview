1731940c20f5f7b936740a8691f3110e9870558f
nemokoala <fdxguy2@gmail.com>
2023-05-21 04:24:05 +0900

    회원가입창 수정중

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -2,23 +2,42 @@ import styled from "styled-components";
 
 function Register() {
   return (
-    <Form>
-      <Title>회원가입</Title>
-      <Label>이메일</Label>
-      <Input type="email" placeholder="Email"></Input>
-    </Form>
+    <Container>
+      <Form>
+        <Title>회원가입</Title>
+        <Label>이메일</Label>
+        <Input type="email" placeholder="Email"></Input>
+        <Label>비밀번호</Label>
+        <Input type="email" placeholder="Email"></Input>
+      </Form>
+    </Container>
   );
 }
 
 export default Register;
-
+const Container = styled.div`
+  width: 100%;
+  height: calc(100vh - var(--navHeight));
+  background: linear-gradient(
+    to bottom,
+    rgb(232, 251, 255),
+    rgb(255, 255, 255),
+    rgb(158, 227, 248)
+  );
+  margin: 0;
+  display: inline-block;
+`;
 const Form = styled.div`
   margin: 100px auto;
   width: 500px;
   height: auto;
   display: flex;
   flex-direction: column;
-  background-color: darkslategray;
+  -webkit-backdrop-filter: blur(15px);
+  backdrop-filter: blur(15px);
+  background-color: rgba(138, 159, 231, 0.226);
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 0px 20px 10px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
   border-radius: 15px;
   padding: 20px 50px;
 `;
@@ -30,6 +49,7 @@ const Title = styled.div`
 const Label = styled.span`
   color: white;
   font-size: 1rem;
+  margin-top: 20px;
 `;
 
 const Input = styled.input`
@@ -38,4 +58,5 @@ const Input = styled.input`
   padding: 10px;
   margin: 5px auto;
   border-radius: 15px;
+  border: 0px;
 `;
