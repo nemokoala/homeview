b869722db14959f557abc2816515d194580111ab
nemokoala <fdxguy2@gmail.com>
2023-06-21 21:00:23 +0900

    30분

--- src/slice/userSlice.tsx
+++ src/slice/userSlice.tsx
@@ -1,14 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+let loadData = JSON.parse(localStorage.getItem("session") as any);
+let now = new Date();
+let diff = now.getTime() - loadData.date.getTime();
+let diffMinutes = Math.floor(diff / 1000 / 60);
+console.log("시간차이" + diffMinutes);
+if (diffMinutes >= 30) {
+  localStorage.removeItem("session");
+  loadData = "";
+}
+
 export const userSlice = createSlice({
   name: "userSet",
   initialState: {
-    session: JSON.parse(sessionStorage.getItem("session") as any),
+    session: JSON.parse(localStorage.getItem("session") as any),
   },
   reducers: {
     saveSession: (state, action: any): any => {
       console.log("reducer : " + JSON.stringify(action.payload));
-      sessionStorage.setItem("session", JSON.stringify(action.payload)); //sessionstoarge에 저장
-      if (action.payload === "") sessionStorage.removeItem("session"); // 값이 비면 세선 삭제
+      const data = { ...action.payload, date: now };
+      localStorage.setItem("session", JSON.stringify(data)); //sessionstoarge에 저장
+      if (action.payload === "") localStorage.removeItem("session"); // 값이 비면 세선 삭제
       state.session = action.payload;
     },
   },
