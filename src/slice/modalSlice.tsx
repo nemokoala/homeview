17f3ca059b5291b7e714bf355f1f86fdb94910c5
nemokoala <fdxguy2@gmail.com>
2023-06-24 03:53:40 +0900

    요류 수정

--- src/slice/modalSlice.tsx
+++ src/slice/modalSlice.tsx
@@ -17,8 +17,11 @@ export const modalSlice = createSlice({
   },
   reducers: {
     setModal: (state: any, action: any): any => {
-      console.log(action.payload);
-      if (action.payload.text.includes("500")) {
+      console.log(JSON.stringify(action.payload));
+      if (
+        typeof action.payload.text === "string" &&
+        action.payload.text.includes("500")
+      ) {
         action.payload.text =
           "로그인 세션이 만료되었습니다. 다시 로그인해주세요.";
       }
