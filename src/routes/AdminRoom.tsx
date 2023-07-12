1c37a329f43a65ce3a089fee39cc9b9544ddaa38
nemokoala <fdxguy2@gmail.com>
2023-07-12 15:48:53 +0900

    방 목록 어드민

--- src/routes/AdminRoom.tsx
+++ src/routes/AdminRoom.tsx
@@ -1,23 +1,62 @@
 import { useState, useEffect } from "react";
 import styles from "./AdminUser.module.css";
+import { apiAddress } from "value";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setModal } from "slice/modalSlice";
 
-function AdminRoom({ reviewData, setReviewData }: any) {
-  const [rooms, setRooms] = useState<any>([...reviewData]);
-
-  const onClickDestroy = (id: string): void => {
+function AdminRoom() {
+  const [rooms, setRooms] = useState<any>([]);
+  const dispatch = useDispatch();
+  const onClickDestroy = async (id: number) => {
     const answer = prompt(
       `해당 방의 아이디("${id}")를 입력하면 삭제처리가 됩니다.`
     );
     if (answer == null) alert("방 삭제를 취소하였습니다.");
-    else if (answer == id) {
-      setRooms((current: any) =>
-        current.filter((room: any) => room.reviewId !== id)
-      );
-      setReviewData((current: any) =>
-        current.filter((room: any) => room.reviewId !== id)
-      );
-    } else if (answer != id)
+    else if (parseInt(answer) === id) {
+      try {
+        const response = await axios.delete(
+          `${apiAddress}/admin/review/${id}`,
+          { withCredentials: true }
+        );
+        getRooms();
+        alert("삭제가 완료되었습니다.");
+        console.log(
+          "AdminRoom.tsx(onClickDestroy): " + JSON.stringify(response)
+        );
+      } catch (error: any) {
+        dispatch(
+          setModal({
+            title: "에러",
+            titleColor: "red",
+            text: JSON.stringify(error),
+          } as any)
+        );
+        console.error(
+          "AdminRoom.tsx(onClickDestroy): " + JSON.stringify(error)
+        );
+      }
+    } else if (parseInt(answer) !== id)
       alert("id값을 잘못 입력하여서 방 삭제가 취소 되었습니다.");
+  };
+
+  useEffect(() => {
+    getRooms();
+  }, []);
+
+  const getRooms = async () => {
+    try {
+      const response = await axios.get(`${apiAddress}/admin/room/list`);
+      setRooms(response.data);
+    } catch (error: any) {
+      dispatch(
+        setModal({
+          title: "에러",
+          titleColor: "red",
+          text: JSON.stringify(error),
+        } as any)
+      );
+    }
   };
   const sortId = (n: number): void => {
     const sortedId = [...rooms].sort((a, b) => {
@@ -77,18 +116,20 @@ function AdminRoom({ reviewData, setReviewData }: any) {
             <th>삭제</th>
           </tr>
           {rooms.map((room: any) => (
-            <tr key={room.reviewId}>
-              <td>{room.reviewId}</td>
+            <tr key={room.room_id}>
+              <td>{room.room_id}</td>
               <td>{room.building}</td>
-              <td>{room.star}</td>
-              <td>{room.newAddress}</td>
+              <td>{room.score}</td>
               <td>
-                {room.lat},{room.lng}
+                {room.new_Address} | {room.old_Address}
+              </td>
+              <td>
+                {room.latitude},{room.longitude}
               </td>
               <td>
                 <button
                   style={{ background: "rgb(228, 84, 84)" }}
-                  onClick={() => onClickDestroy(room.reviewId)}
+                  onClick={() => onClickDestroy(room.room_id)}
                 >
                   삭제
                 </button>
